cc4ebb47dc6373c8577273e375a4678686afee75
Mohammad Alali <alalim4@my.erau.edu>
2023-08-04 23:21:10 +0300

    Update Texture

--- Camel/camel/Texture.cpp
+++ Camel/camel/Texture.cpp
@@ -1,53 +1,56 @@
 #include "Texture.h"
 
-#include <iostream>
 #include "vendor/stb_image/stb_image.h"
 
-Texture::Texture(const std::string& path, const FilterMode filterMode)
-	: m_TextureID(0), m_FilePath(path), m_Width(0), m_Height(0), m_BPP(0)
+namespace Camel
 {
-	stbi_set_flip_vertically_on_load(1);
-	auto imageBuffer = stbi_load(path.c_str(), &m_Width, &m_Height, &m_BPP, 4);
-	if (!imageBuffer)
+	Texture::Texture(const std::string& filePath, const FilterMode filterMode)
+		: m_TextureID(0), m_Width(0), m_Height(0), m_NumChannels(0)
 	{
-		std::cout << "Failed to load texture image from path: " << path << std::endl;
+		stbi_set_flip_vertically_on_load(1);
+		auto imageBuffer = stbi_load(filePath.c_str(), &m_Width, &m_Height, &m_NumChannels, 4);
+		if (!imageBuffer)
+		{
+			CAMEL_LOG_ERROR("Failed to load texture from path: {}", filePath);
+			throw std::runtime_error("Failed to load texture from path: " + filePath);
+		}
+
+		glGenTextures(1, &m_TextureID);
+		glBindTexture(GL_TEXTURE_2D, m_TextureID);
+
+		// Minification filter
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, (GLenum)filterMode);
+
+		// Magnification filter
+		if (filterMode == FilterMode::LINEAR || filterMode == FilterMode::LINEAR_MIPMAP_NEAREST || filterMode == FilterMode::LINEAR_MIPMAP_LINEAR)
+		{
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		}
+		else
+		{
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		}
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, m_Width, m_Height, 0, GL_RGBA, GL_UNSIGNED_BYTE, imageBuffer);
+
+		// Generate mipmaps
+		if (filterMode == FilterMode::NEAREST_MIPMAP_NEAREST || filterMode == FilterMode::LINEAR_MIPMAP_NEAREST || filterMode == FilterMode::NEAREST_MIPMAP_LINEAR || filterMode == FilterMode::LINEAR_MIPMAP_LINEAR)
+		{
+			glGenerateMipmap(GL_TEXTURE_2D);
+		}
+
+		// Unbind
+		glBindTexture(GL_TEXTURE_2D, 0);
+
+		if (imageBuffer)
+			stbi_image_free(imageBuffer);
 	}
 
-	glGenTextures(1, &m_TextureID);
-	glBindTexture(GL_TEXTURE_2D, m_TextureID);
-
-	// Minification filter
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, (GLenum)filterMode);
-
-	// Magnification filter
-	if (filterMode == FilterMode::LINEAR || filterMode == FilterMode::LINEAR_MIPMAP_NEAREST || filterMode == FilterMode::LINEAR_MIPMAP_LINEAR)
+	Texture::~Texture() noexcept
 	{
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glDeleteTextures(1, &m_TextureID);
 	}
-	else
-	{
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	}
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, m_Width, m_Height, 0, GL_RGBA, GL_UNSIGNED_BYTE, imageBuffer);
-
-	// Generate mipmaps
-	if (filterMode == FilterMode::NEAREST_MIPMAP_NEAREST || filterMode == FilterMode::LINEAR_MIPMAP_NEAREST || filterMode == FilterMode::NEAREST_MIPMAP_LINEAR || filterMode == FilterMode::LINEAR_MIPMAP_LINEAR)
-	{
-		glGenerateMipmap(GL_TEXTURE_2D);
-	}
-
-	// Unbind
-	glBindTexture(GL_TEXTURE_2D, 0);
-
-	if (imageBuffer)
-		stbi_image_free(imageBuffer);
-}
-
-Texture::~Texture() noexcept
-{
-	glDeleteTextures(1, &m_TextureID);
 }
--- Camel/camel/Texture.h
+++ Camel/camel/Texture.h
@@ -1,43 +1,43 @@
 #pragma once
 
-#include <string>
-#include <GL/glew.h>
+#include "Core.h"
 
-class Texture
+namespace Camel
 {
-public:
-	enum class FilterMode
+	class Texture
 	{
-		NEAREST = GL_NEAREST,
-		LINEAR = GL_LINEAR,
-		NEAREST_MIPMAP_NEAREST = GL_NEAREST_MIPMAP_NEAREST,
-		LINEAR_MIPMAP_NEAREST = GL_LINEAR_MIPMAP_NEAREST,
-		NEAREST_MIPMAP_LINEAR = GL_NEAREST_MIPMAP_LINEAR,
-		LINEAR_MIPMAP_LINEAR = GL_LINEAR_MIPMAP_LINEAR
+	public:
+		enum class FilterMode
+		{
+			NEAREST = GL_NEAREST,
+			LINEAR = GL_LINEAR,
+			NEAREST_MIPMAP_NEAREST = GL_NEAREST_MIPMAP_NEAREST,
+			LINEAR_MIPMAP_NEAREST = GL_LINEAR_MIPMAP_NEAREST,
+			NEAREST_MIPMAP_LINEAR = GL_NEAREST_MIPMAP_LINEAR,
+			LINEAR_MIPMAP_LINEAR = GL_LINEAR_MIPMAP_LINEAR
+		};
+
+		Texture(const std::string& filePath, const FilterMode filterMode = FilterMode::LINEAR);
+		~Texture() noexcept;
+
+		inline void Bind(unsigned int slot = 0) const noexcept
+		{
+			CAMEL_ASSERT(slot >= 0 && slot <= 31, "Cannot bind texture to slot {}. Acceptable values are 0 to 31.", slot);
+			glActiveTexture(GL_TEXTURE0 + slot);
+			glBindTexture(GL_TEXTURE_2D, m_TextureID);
+		}
+
+		inline void Unbind() const noexcept
+		{
+			glBindTexture(GL_TEXTURE_2D, 0);
+		}
+
+		inline int GetWidth() const noexcept { return m_Width; }
+		inline int GetHeight() const noexcept { return m_Height; }
+		inline int GetNumChannels() const noexcept { return m_NumChannels; }
+
+	private:
+		unsigned int m_TextureID;
+		int m_Width, m_Height, m_NumChannels;
 	};
-
-	Texture(const std::string& path, const FilterMode filterMode = FilterMode::LINEAR);
-	~Texture() noexcept;
-
-	void Bind(unsigned int slot = 0) const noexcept
-	{
-		if (slot < 0 || slot > 31)
-			slot = 0;
-		glActiveTexture(GL_TEXTURE0 + slot);
-		glBindTexture(GL_TEXTURE_2D, m_TextureID);
-	}
-
-	void Unbind() const noexcept
-	{
-		glBindTexture(GL_TEXTURE_2D, 0);
-	}
-
-
-	inline int GetWidth() const noexcept { return m_Width; }
-	inline int GetHeight() const noexcept { return m_Height; }
-
-private:
-	unsigned int m_TextureID;
-	std::string m_FilePath;
-	int m_Width, m_Height, m_BPP;
-};
+}
