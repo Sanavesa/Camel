94f586d2708494f1fa46cc63b0932a461f798f42
Mohammad Alali <alalim4@my.erau.edu>
2023-08-05 22:41:40 +0300

    Update project

--- Camel/Main.cpp
+++ Camel/Main.cpp
@@ -3,10 +3,9 @@
 // 4. UI solution via ImGUI
 // 5. Can I move the mesh vertices dynamically?
 // 6. Add a renderer that encapsulates shit
-// 7. Add a window that encapsulates shit
 // 8. Add resource manager (for mesh, texture, shader, etc)
 // 9. add MeshAsset and MeshInstance?
-// 10. Add light class
+// 10. Add Scene
 
 
 #include "camel/Core.h"
@@ -19,6 +18,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <GL/glew.h>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_opengl.h>
 
 #include "camel/Mesh.h"
 #include "camel/Shader.h"
@@ -26,134 +26,131 @@
 #include "camel/Light.h"
 #include "camel/Camera.h"
 #include "camel/MeshLoader.h"
+#include "camel/Input.h"
+#include "camel/Application.h"
 
-constexpr int WIDTH = 800;
-constexpr int HEIGHT = 600;
+using namespace Camel;
 
-int main(int argc, char* argv[])
+class SimpleApp : public Application
 {
-	SDL_Init(SDL_INIT_VIDEO);
-	SDL_Window* window = SDL_CreateWindow("OpenGL", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_OPENGL);
-	SDL_GLContext context = SDL_GL_CreateContext(window);
+public:
+	SimpleApp(const int width, const int height, const std::string& title)
+		: Application(width, height, title)
+	{}
 
-	glewInit();
-
-	std::cout << "GL Version: " << glGetString(GL_VERSION) << std::endl;
-
-	// Enable blending
-	glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-	// Enable depth test
-	glEnable(GL_DEPTH_TEST);
-
-	// Enable face culling
-	glEnable(GL_CULL_FACE);
-	glFrontFace(GL_CW); // Left Handedness
-
-	Camel::Mesh mesh = Camel::MeshLoader::Load("res/models/sword.obj");
-	Camel::Transform meshTransform;
-	meshTransform.Translate(0, 0, 5);
-
-	Camel::Shader shader("res/shaders/Diffuse_vert.shader", "res/shaders/Diffuse_frag.shader");
-	shader.Bind();
-
-	Camel::Texture texture("res/textures/palette.png", Camel::Texture::FilterMode::NEAREST);
-	texture.Bind();
-
-	Camel::Camera camera(glm::vec3(0.0f, 0.0f, 0.0f), glm::quat(glm::vec3(0.0f, 0, 0.0f)), 90.0f, (float)WIDTH / HEIGHT);
-	camera.GetTransform().LookAt(meshTransform.GetPosition());
-
-	Camel::Light light(glm::vec3(5.0f, 10.0f, -5.0f), glm::vec3(1.0f, 0.95f, 0.9f));
-
-	SDL_Event windowEvent;
-	while (true)
+	~SimpleApp() override
 	{
-		if (SDL_PollEvent(&windowEvent))
-		{
-			if (windowEvent.type == SDL_QUIT)
-				break;
-			if (windowEvent.type == SDL_KEYDOWN)
-			{
-				if (windowEvent.key.keysym.sym == SDLK_ESCAPE)
-					break;
+		delete m_Shader;
+		delete m_Mesh;
+		delete m_MeshTransform;
+		delete m_Texture;
+		delete m_Camera;
+		delete m_Light;
+	}
 
-				switch (windowEvent.key.keysym.sym)
-				{
-				case SDLK_w:
-					camera.GetTransform().Translate(camera.GetTransform().GetForward() * 0.1f);
-					break;
-				case SDLK_s:
-					camera.GetTransform().Translate(camera.GetTransform().GetBack() * 0.1f);
-					break;
-				case SDLK_a:
-					camera.GetTransform().Translate(camera.GetTransform().GetLeft() * 0.1f);
-					break;
-				case SDLK_d:
-					camera.GetTransform().Translate(camera.GetTransform().GetRight() * 0.1f);
-					break;
-				case SDLK_SPACE:
-					camera.GetTransform().Translate(camera.GetTransform().GetUp() * 0.1f);
-					break;
-				case SDLK_LSHIFT:
-					camera.GetTransform().Translate(camera.GetTransform().GetDown() * 0.1f);
-					break;
-				case SDLK_q:
-					camera.GetTransform().Rotate(0, -0.01f, 0);
-					break;
-				case SDLK_e:
-					camera.GetTransform().Rotate(0, 0.01f, 0);
-					break;
-				case SDLK_LEFT:
-					light.GetTransform().Translate(light.GetTransform().GetLeft() * 0.1f);
-					break;
-				case SDLK_RIGHT:
-					light.GetTransform().Translate(light.GetTransform().GetRight() * 0.1f);
-					break;
-				case SDLK_UP:
-					light.GetTransform().Translate(light.GetTransform().GetUp() * 0.1f);
-					break;
-				case SDLK_DOWN:
-					light.GetTransform().Translate(light.GetTransform().GetDown() * 0.1f);
-					break;
-				case SDLK_z:
-					light.GetTransform().Translate(light.GetTransform().GetUp() * 0.1f);
-					break;
-				case SDLK_x:
-					light.GetTransform().Translate(light.GetTransform().GetDown() * 0.1f);
-					break;
-				}
-			}
-		}
+	virtual void OnStart() override
+	{
+		m_Shader = new Shader("res/shaders/Diffuse_vert.shader", "res/shaders/Diffuse_frag.shader");
+		m_Shader->Bind();
 
-		meshTransform.Rotate(0.03f, 0.05f, -0.07f);
+		m_Mesh = new Mesh(MeshLoader::Load("res/models/sword.obj"));
+		m_MeshTransform = new Transform(glm::vec3(0, 0, 5));
+
+		m_Texture = new Texture("res/textures/palette.png", Camel::Texture::FilterMode::NEAREST);
+		m_Texture->Bind();
+
+		m_Camera = new Camera(glm::vec3(0.0f, 0.0f, 0.0f), glm::quat(glm::vec3(0.0f, 0, 0.0f)), 90.0f, GetAspectRatio());
+		m_Camera->GetTransform().LookAt(m_MeshTransform->GetPosition());
+
+		m_Light = new Light(glm::vec3(5.0f, 10.0f, -5.0f), glm::vec3(1.0f, 0.95f, 0.9f));
+	}
+
+	virtual void OnUpdate(float deltaTime) override
+	{
+		const Input input = GetInput();
+
+		if (input.GetKey(SDL_SCANCODE_ESCAPE))
+			Quit();
+
+		if (input.GetKey(SDL_SCANCODE_W))
+			m_Camera->GetTransform().Translate(m_Camera->GetTransform().GetForward() * 0.1f);
+
+		if (input.GetKey(SDL_SCANCODE_S))
+			m_Camera->GetTransform().Translate(m_Camera->GetTransform().GetBack() * 0.1f);
+
+		if (input.GetKey(SDL_SCANCODE_A))
+			m_Camera->GetTransform().Translate(m_Camera->GetTransform().GetLeft() * 0.1f);
+
+		if (input.GetKey(SDL_SCANCODE_D))
+			m_Camera->GetTransform().Translate(m_Camera->GetTransform().GetRight() * 0.1f);
+
+		if (input.GetKey(SDL_SCANCODE_SPACE))
+			m_Camera->GetTransform().Translate(m_Camera->GetTransform().GetUp() * 0.1f);
+
+		if (input.GetKey(SDL_SCANCODE_LSHIFT))
+			m_Camera->GetTransform().Translate(m_Camera->GetTransform().GetDown() * 0.1f);
+
+		if (input.GetKey(SDL_SCANCODE_Q))
+			m_Camera->GetTransform().Rotate(0, -0.05f, 0);
+
+		if (input.GetKey(SDL_SCANCODE_E))
+			m_Camera->GetTransform().Rotate(0, 0.05f, 0);
+
+		if (input.GetKey(SDL_SCANCODE_LEFT))
+			m_Light->GetTransform().Translate(m_Light->GetTransform().GetLeft() * 0.1f);
+
+		if (input.GetKey(SDL_SCANCODE_RIGHT))
+			m_Light->GetTransform().Translate(m_Light->GetTransform().GetRight() * 0.1f);
+
+		if (input.GetKey(SDL_SCANCODE_UP))
+			m_Light->GetTransform().Translate(m_Light->GetTransform().GetUp() * 0.1f);
+
+		if (input.GetKey(SDL_SCANCODE_DOWN))
+			m_Light->GetTransform().Translate(m_Light->GetTransform().GetDown() * 0.1f);
+
+		if (input.GetKey(SDL_SCANCODE_Z))
+			m_Light->GetTransform().Translate(m_Light->GetTransform().GetUp() * 0.1f);
+
+		if (input.GetKey(SDL_SCANCODE_X))
+			m_Light->GetTransform().Translate(m_Light->GetTransform().GetDown() * 0.1f);
+
+		m_MeshTransform->Rotate(0.03f, 0.05f, -0.07f);
 
 		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		shader.SetUniform1i("u_DiffuseImage", 0);
+		m_Shader->SetUniform1i("u_DiffuseImage", 0);
 
-		shader.SetUniformMatrix4f("u_Model", meshTransform.GetMatrix());
-		shader.SetUniformMatrix4f("u_View", camera.GetViewMatrix());
-		shader.SetUniformMatrix4f("u_Projection", camera.GetProjectionMatrix());
+		m_Shader->SetUniformMatrix4f("u_Model", m_MeshTransform->GetMatrix());
+		m_Shader->SetUniformMatrix4f("u_View", m_Camera->GetViewMatrix());
+		m_Shader->SetUniformMatrix4f("u_Projection", m_Camera->GetProjectionMatrix());
 
-		shader.SetUniform3f("u_LightPos", light.GetTransform().GetPosition());
-		shader.SetUniform3f("u_LightColor", light.GetColor());
+		m_Shader->SetUniform3f("u_LightPos", m_Light->GetTransform().GetPosition());
+		m_Shader->SetUniform3f("u_LightColor", m_Light->GetColor());
 
-		shader.SetUniform3f("u_ViewPos", camera.GetTransform().GetPosition());
+		m_Shader->SetUniform3f("u_ViewPos", m_Camera->GetTransform().GetPosition());
 
-		shader.SetUniform3f("u_SkyColor", 0.3f, 0.7f, 1.0f);
-		shader.SetUniform3f("u_GroundColor", 0.5f, 0.4f, 0.3f);
-		shader.SetUniform3f("u_BaseColor", 1.0f, 1.0f, 1.0f);
+		m_Shader->SetUniform3f("u_SkyColor", 0.3f, 0.7f, 1.0f);
+		m_Shader->SetUniform3f("u_GroundColor", 0.5f, 0.4f, 0.3f);
+		m_Shader->SetUniform3f("u_BaseColor", 1.0f, 1.0f, 1.0f);
 
-		mesh.Draw();
-
-		SDL_GL_SwapWindow(window);
+		m_Mesh->Draw();
 	}
 
-	SDL_GL_DeleteContext(context);
-	SDL_DestroyWindow(window);
-	SDL_Quit();
+private:
+	// TODO: Temporary ghetto raw pointers
+	Shader* m_Shader = nullptr;
+	Camera* m_Camera = nullptr;
+	Mesh* m_Mesh = nullptr;
+	Transform* m_MeshTransform = nullptr;
+	Texture* m_Texture = nullptr;
+	Light* m_Light = nullptr;
+};
+
+int main(int argc, char* argv[])
+{
+	SimpleApp app(1280, 720, "Camel");
+	app.Run();
 
 	return 0;
 }
