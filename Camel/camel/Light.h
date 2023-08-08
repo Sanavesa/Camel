b79a41669aefdd1b362f5b70b70378f8af14f635
Mohammad Alali <alalim4@my.erau.edu>
2023-08-08 23:02:05 +0300

    Update Light.h

--- Camel/camel/Light.h
+++ Camel/camel/Light.h
@@ -5,12 +5,31 @@
 
 namespace Camel
 {
-	class Light
+	class Light final
 	{
 	public:
 		Light(const glm::vec3& position = glm::vec3(0.0f), const glm::vec3& color = glm::vec3(1.0f))
 			: m_Transform(position), m_Color(color)
 		{}
+
+		Light(const Light&) = delete;
+		Light& operator=(const Light&) = delete;
+
+		Light(Light&& other) noexcept
+			: m_Transform(std::move(other.m_Transform)), m_Color(std::move(other.m_Color))
+		{}
+
+		Light& operator=(Light&& other) noexcept
+		{
+			if (this != &other)
+			{
+				m_Transform = std::move(other.m_Transform);
+				m_Color = std::move(other.m_Color);
+			}
+			return *this;
+		}
+
+		~Light() = default;
 
 		inline const Transform& GetTransform() const noexcept { return m_Transform; }
 		inline Transform& GetTransform() noexcept { return m_Transform; }
